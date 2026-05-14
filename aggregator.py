87c81e567aa3fb7eaa0447fb98fc42a7d2ca7f90
Axel Marsacq <axel.marsacq2@mail.dcu.ie>
2026-05-14 16:54:54 +0200

    Local Ping Pong

--- aggregator.py
+++ aggregator.py
@@ -1,3 +1,38 @@
+import socket
+
 class Aggregator :
     def __init__(self):
         pass
+
+    
+    def start_server(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            s.bind(('0.0.0.0', 65432))
+            s.listen()
+            print(f"Server listening on {'0.0.0.0'}:{65432}...")
+            
+            # This line makes the program "pause" and wait. It sits there until someone tries to connect
+            conn, addr = s.accept()
+
+            # Python will automatically close the connection for you once the code inside that block finishes, even if the program crashes
+            with conn:
+
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    message = data.decode('utf-8')
+                    print(f"Received : {message}")
+                    
+                    if message.lower() == "ping":
+                        conn.sendall(b"Pong")
+                    else:
+                        conn.sendall(b"Message received")
+        except KeyboardInterrupt:
+            print("\nStopping...")
+
+ag = Aggregator()
+
+ag.start_server()
--- node.py
+++ node.py
@@ -1,3 +1,27 @@
+import socket
+
 class Node :
     def __init__(self):
         pass
+
+    def start_client(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            s.connect(('192.168.1.34', 65432))
+            print(f"Connecté au serveur {'192.168.1.34'}")
+                
+           
+            print("Envoi : Ping")
+            s.sendall(b"Ping")
+            
+            data = s.recv(1024)
+            print(f"Reçu du serveur : {data.decode('utf-8')}")
+                
+                
+        except KeyboardInterrupt:
+            print("\nDéconnexion...")
+        except ConnectionRefusedError:
+            print("Error : Connection refused")
+
+n = Node()
+n.start_client()
