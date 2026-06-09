ca77de00e99b4118092ebe068be36ef064e4d0b4
Axel Marsacq <axel.mrsq@gmail.com>
2026-06-09 16:32:16 +0200

    Printing apparent power value of an entity in appadeamon log

--- externalpi/homeassistantcom_test.py
+++ externalpi/homeassistantcom_test.py
@@ -5,8 +5,10 @@ import socket
 class HelloWorld(hass.Hass):
     def initialize(self):
         self.log("commonication !")
-        self.listen_state(self.ping_callback,"input_button.ping")
+        # self.listen_state(self.ping_callback,"input_button.ping")
         # https://appdaemon.readthedocs.io/en/3.0.0/APIREFERENCE.html#listen-state
+        self.log(self.get_state("sensor.lixee_zlinky_tic_puissance_apparente"))
+        # https://appdaemon.readthedocs.io/en/3.0.0/APIREFERENCE.html#get-state
         
 
     def ping_callback(self, entity, attribute, old, new, kwargs):
@@ -18,4 +20,5 @@ class HelloWorld(hass.Hass):
         s.sendall(b"ping")
         self.log("Waiting for answer")
         self.log(s.recv(4096).decode("utf-8"))
+        
         
