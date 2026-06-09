c30a5bc5c2441679610600960a7ef3c18d2132eb
Axel Marsacq <axel.mrsq@gmail.com>
2026-06-09 14:39:25 +0200

    Com test homeassistant to external with socket OK

---
+++ externalpi/homeassistantcom_test.py
@@ -0,0 +1,12 @@
+import socket
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.bind(('0.0.0.0', 65400))
+s.listen()
+
+conn, addr = s.accept()
+           
+encoded_data = conn.recv(4096)
+
+print(encoded_data.decode("utf-8"))
---
+++ homeassistantpi/com.py
@@ -0,0 +1,10 @@
+import appdaemon.plugins.hass.hassapi as hass
+import socket
+
+
+class HelloWorld(hass.Hass):
+    def initialize(self):
+        self.log("commonucation !")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        s.connect(("192.168.1.44", 65400))
+        s.sendall(b"coucou")
