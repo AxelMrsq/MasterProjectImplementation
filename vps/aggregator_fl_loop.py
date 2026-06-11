2262bdfbc5e76823ee650a43ccea307af4460bda
Axel Marsacq <axel.mrsq@gmail.com>
2026-06-11 22:25:29 +0200

    Starting aggregator FL loop

---
+++ vps/aggregator_fl_loop.py
@@ -0,0 +1,29 @@
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import LSTM, Input, Dense
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import backend 
+from pickle import loads, dumps
+from pandas import read_csv
+from numpy import array
+import socket
+
+
+
+def initiateGlobalModel():
+    model = Sequential()
+    model.add(Input(shape = (24, 5)))  
+    model.add(LSTM(32, return_sequences=True))
+    model.add(LSTM(16))
+    model.add(Dense(1)) 
+    model.save("Global_model.keras") 
+
+def start():
+    initiateGlobalModel()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.bind(('0.0.0.0', 65432))
+    s.listen()
+
+
+    conn, addr = s.accept()
