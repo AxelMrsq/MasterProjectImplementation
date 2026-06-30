6addb913742ee5e4065ab05f55922caa412c4402
Axel Marsacq <axel.mrsq@gmail.com>
2026-06-30 16:40:38 +0200

    Specific data extraction

--- homeassistantpi/other_features.py
+++ homeassistantpi/other_features.py
@@ -6,14 +6,23 @@ class Train(hass.Hass):
         # self.log("Waiting for a ping...")
         # self.listen_state(self.ping_callback, "input_button.ping")
         self.weather_entity =  self.get_entity("weather.forecast_home")
-        state = self.weather_entity.get_state(attribute="all")
-        self.log(state)
+        temperature = self.weather_entity.get_state(attribute="temperature")
+        humidity = self.weather_entity.get_state(attribute="humidity")
+        wind_speed = self.weather_entity.get_state(attribute="wind_speed") #m/s
+        
+        self.log("temperature : ")
+        self.log(temperature)
+        self.log("humidity : ")
+        self.log(humidity)
+        self.log("wind speed m/s : ")
+        self.log(wind_speed)
+        self.log("wind speed km/h : ")
+        self.log(wind_speed*3.6)
+
         
 
     def ping_callback(self, entity, attribute, old, new, kwargs):
         # self.log("Ping received! Gathering 30 days of data...")
         pass
-        
-        
         
         
