117c328d3f52202b1a17670c48fb724496151857
Axel Marsacq <axel.mrsq@gmail.com>
2026-07-24 10:13:41 +0200

    Debug wind speed km/h to m/s

--- homeassistantpi/train.py
+++ homeassistantpi/train.py
@@ -126,7 +126,7 @@ class Train(hass.Hass):
                 if "humidity" in attrs and attrs["humidity"] is not None:
                     humidity_groups[hour_floor].append(float(attrs["humidity"]))
                 if "wind_speed" in attrs and attrs["wind_speed"] is not None:
-                    wind_groups[hour_floor].append(float(attrs["wind_speed"]))
+                    wind_groups[hour_floor].append(float(attrs["wind_speed"])/3.6)
 
         # Assemble timelines
         all_timestamps = list(consumption_groups.keys()) + list(temp_groups.keys())
