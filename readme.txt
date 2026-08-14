f5d4b851c714497a71702b49a189a94ecb07f408
Axel Marsacq <axel.mrsq@gmail.com>
2026-08-14 14:43:11 +0200

    end

--- readme.txt
+++ readme.txt
@@ -1,5 +1,80 @@
-Master project Github repository
+------INTRODUCTION------
 
-Regroup the code which will run on raspberry pi as local node and on a virtual private server as aggregator
+This repository contains the core items of a Master Project implementation. The main goal was to reproduce in real condition a Federated Learning architecture proposed through a simulation in the literature in order to evaluate its potential and better understand how a federated system could be built.
 
-tf_env need to be a 3.11 python version environment for tensorflow
+
+------STRUCTURE------
+
+A00057317_Axel_Marsacq_project_repository:.
+├───demo
+│   ├───final_demo_inference_training50rounds
+│           final_demo_10_08_26_compressed.mp4
+│           
+├───docs
+│       aggregator_loop.png
+│       data_collection_system_schematic.pdf
+│       global_system_schematic.pdf
+│       twin_raspberry_system_schematic.pdf
+│       
+├───results
+│   └───data_collection
+│           AP_data_sample_home_1.csv
+│           AP_data_sample_home_2.csv
+│           
+├───src
+│   ├───home_assistant_AppDaemon
+│   │       infer.py
+│   │       train.py
+│   │       
+│   ├───second_raspberry_pi
+│   │       nodeflloop.py
+│   │       requirements.txt
+│   │       
+│   └───virtual_private_server
+│           aggregator_fl_loop.py
+│           requirements.txt
+│           
+└───testing
+    ├───cluster_algo_eval
+    │       clustertest.txt
+    │       
+    └───node_simulation
+        │   node_fake.py
+        │   proto_data.csv
+        │   
+        │       
+        └───federated_training_simulation_with_two_fake_nodes_and_vps
+                video.mp4
+
+
+------COMPONENTS------
+
+The repository is divided into 5 folders. 
+
+The main one is -src, it contains all the python scipts needed to run the implementation. The three sub-folders separate the different scripts by the hardware on which they are running. 
+
+The -demo folder regroups different demonstrations of the working system.
+
+The -docs folder brings together implementation and design details.
+
+The -testing folder contains containing data, code and tool which can be used to simulate some part of the system or evaluate it. (Need to create a python virtual environment with scikit-learn, numpy and matplotlib)
+
+The -results folder merges data used in the Master project conference paper.
+
+
+------USER GUIDE------
+
+To execute the implementation the user will need three main components : a virtual private server with Ubuntu as operating system, a raspberry pi with the Home Assistant operating system and another raspberry pi with a Raspberry Pi operating system previously called Raspbian.
+
+On the virtual private server, the user need to install the tensorflow and numpy packages to execute the aggregator_fl_loop.py python scripts when doing a Federated Training.
+
+For the Home Assistant set up, the AppDaemon add-on need to be install alongside the Studio Code Server add-on. The two scripts train.py and infer.py need to be imported in AppDaemon as apps through the Studio Code Server UI. Then, the numpy package must be added into the configuration of AppDaemon. Finally, AppDaemon need to be started and the user starts either a training or an inference procedure by creating and using a start_training or making_inference helper button entity on the Home Assistant dashboard.
+
+Python 3.11 needs to be installed on the second raspberry pi to enable the use of the package tensorflow. The numpy package must also be installed to enable the nodeflloop.py script to run on the raspberry. The systemd tool of ubuntu can be used to run the script at each boot the second raspberry.
+
+
+
+
+
+
+
---
+++ testing/node_simulation/node_fake.py
@@ -0,0 +1,177 @@
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import LSTM, Input, Dense
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import backend 
+from pickle import loads, dumps
+import pandas
+from pandas import read_csv
+from numpy import array
+import socket
+import struct
+
+def initiateLocalModel():
+    model = Sequential()
+    model.add(Input(shape = (24, 5)))  
+    model.add(LSTM(32, return_sequences=True))
+    model.add(LSTM(16))
+    model.add(Dense(1)) 
+    model.save("local_modelbis.keras") 
+
+    backend.clear_session()
+
+
+def loadData(path) :
+    # 1. Read data
+    data = read_csv(path, sep=";")
+
+    features_col = ["Consumption", "Weekday", "Hour", "AVG4D (kWh)", "TempCluster"]
+    target_col = "Consumption"
+
+    features_list = []
+    targets_list = []
+
+    # 2. Populate lists directly (skipping the heavy dictionary structure)
+    for i in range(0, len(data) - 1):
+        features_list.append(data.loc[i-23:i][features_col].values) # .values extracts raw numpy array
+        targets_list.append(data.loc[i+1][target_col])
+    
+    # 3. Calculate split indices
+    total_samples = len(features_list)
+    split_80 = int(total_samples * 0.80)
+    split_85 = int(total_samples * 0.85)
+    split_90 = int(total_samples * 0.90)
+
+    # 4. Generate final arrays
+    X_train = pad_sequences(features_list[:split_85], padding='pre', dtype='float32')
+    X_val = pad_sequences(features_list[split_80:split_90], padding='pre', dtype='float32')
+    X_test = pad_sequences(features_list[split_90:], padding='pre', dtype='float32')
+
+    y_train = array(targets_list[:split_85], dtype='float32')
+    y_val = array(targets_list[split_80:split_90], dtype='float32')
+    y_test = array(targets_list[split_90:], dtype='float32')
+
+    # 5. Return the clean arrays
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+
+def start(port):
+    print("Initiating local model...")
+    initiateLocalModel()
+    print("Check")
+
+
+    print("\nSaving local data...")
+    X_train, X_val, X_test, y_train, y_val, y_test = loadData("proto_data.csv")
+    print("check")
+    
+    print("\nCreating socket")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    print("check")
+
+    print("\nconnecting to socket")
+    s.connect(("83.228.240.76", port))
+    print("check")
+    
+    print("\n send GET cmd")
+    
+    message = dumps({"id":1,"cmd":"GET", "key":True, "value":None})
+    header = struct.pack('!I', len(message))
+
+    s.sendall(header + message)
+    print("check")
+
+    local_model = load_model("local_modelbis.keras")
+    
+    for i in range(50) :
+
+        print("\n receive POST cmd and  set global to local")
+
+        buffer = b""
+        while len(buffer) < 4:
+            packet = s.recv(4 - len(buffer))
+            buffer += packet
+        
+        header = buffer
+        message_length = struct.unpack('!I', header)[0]
+
+        buffer = b""
+        while len(buffer) < message_length:
+            packet = s.recv(message_length - len(buffer))
+            buffer += packet
+        full_data = buffer
+
+
+        local_model.set_weights(loads(full_data)["value"])
+
+        print("check")
+        print("\n evaluate")
+        local_model.compile(optimizer=SGD(learning_rate=0.0001) , loss='mse')
+        local_model.evaluate(X_val, y_val)
+        print("check")
+        print("\n train")
+        
+         
+        local_model.fit(x=X_train, y=y_train, epochs=5, batch_size = 100) 
+        print("check")
+        print("\n evaluate")
+        local_model.evaluate(X_val, y_val)
+        print("check")
+        if i == 49 :
+            print("\n send POST cmd")
+            message = dumps({"id":1,"cmd":"POST", "key": False, "value":local_model.get_weights()})
+            header = struct.pack('!I', len(message))
+
+            s.sendall(header + message) 
+            
+            print("check")
+        else :
+            print("\n send POST cmd") 
+            message = dumps({"id":1,"cmd":"POST", "key": True, "value":local_model.get_weights()})
+            header = struct.pack('!I', len(message))
+
+            s.sendall(header + message)
+            print("check")
+
+    print("\n set global to local")
+
+    buffer = b""
+    while len(buffer) < 4:
+        packet = s.recv(4 - len(buffer))
+        buffer += packet
+    
+    header = buffer
+    message_length = struct.unpack('!I', header)[0]
+
+    buffer = b""
+    while len(buffer) < message_length:
+        packet = s.recv(message_length - len(buffer))
+        buffer += packet
+    full_data = buffer
+
+
+    local_model.set_weights(loads(full_data)["value"])
+
+    print("check")
+
+    s.close()
+    print("\n evaluate")
+    local_model.evaluate(X_val, y_val)
+    print("check")
+    print("\n train")
+
+    local_model.compile(optimizer=SGD(learning_rate=0.0001) , loss='mse')
+
+    local_model.fit(x=X_train, y=y_train, epochs=5, batch_size = 100) 
+    print("check")
+    print("\n evaluate")
+    local_model.evaluate(X_test, y_test)
+    print("check")
+    
+    print("\n save")
+    local_model.save("local_model.keras")
+    print("check")
+    
+    backend.clear_session()
+
+start(65432)
